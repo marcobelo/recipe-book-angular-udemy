66f23eeabf9d313987af607443b70a0428c992d8
Marco Belo <marco.barone.belo@gmail.com>
2024-08-26 23:02:34 -0300

    feat: change EventEmitter to Observables

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
-import {Component} from '@angular/core';
-import {Recipe} from "./recipe.model";
+import {Component, OnInit} from '@angular/core';
 import {RecipeService} from "./recipe.service";
 
 @Component({
@@ -8,19 +7,8 @@ import {RecipeService} from "./recipe.service";
   styleUrl: './recipes.component.css',
   providers: [RecipeService]
 })
-export class RecipesComponent {
-  selectedRecipe: Recipe;
+export class RecipesComponent implements OnInit{
+  constructor() {}
 
-  constructor(private recipeService: RecipeService) {
-
-  }
-
-  ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
-  }
+  ngOnInit() {}
 }
